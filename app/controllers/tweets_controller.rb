d71b874c192461ecc7ab953b2fbda2e8c9d7d874
nkizito92 <nkizito57@gmail.com>
2020-01-19 20:23:05 -0600

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,17 +1,17 @@
 class TweetsController < ApplicationController
 
-    get "/index" do 
-        @tweets = Tweet.all
-        redirect "/tweets"
+    get "/tweets" do 
+        
+        erb :"/tweets/tweets"
     end 
 
-    get "/tweets/new" do 
-        erb :"/tweets/new"
-    end 
+    # get "/tweets/new" do 
+    #     erb :"/tweets/new"
+    # end 
 
-    post "/tweets/:id" do 
-        @user = User.find_by(username: params[:username])
-        @tweet = Tweet.create(:content => params[:content])
-        redirect "/tweets#{@tweet.id}"
-    end 
+    # post "/tweets/:id" do 
+    #     @user = User.find_by(username: params[:username])
+    #     @tweet = Tweet.create(:content => params[:content])
+    #     redirect "/tweets#{@tweet.id}"
+    # end 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,8 +16,8 @@ class UsersController < ApplicationController
     post '/signup' do 
         @user = User.new(:username => params[:username], :password => params[:password], :email => params[:email])
         @user.save
-        session[:user_id] = @user.id
         if !params[:username].empty? && !params[:email].empty? && !params[:password].empty?
+            session[:user_id] = @user.id
             redirect "/tweets"
         else
             redirect "/signup"
@@ -25,13 +25,27 @@ class UsersController < ApplicationController
     end
 
     get '/login' do 
-          erb :"/users/login"
+        if logged_in?
+            redirect "/tweets"
+        else 
+            erb :"/users/login"
+        end
     end 
     
     post '/login' do 
         @user = User.find_by(username: params[:username])
-		redirect "/tweets"
+        if @user && @user.authenticate(params[:password])
+			session[:user_id] = @user.id
+			redirect to "/tweets"
+		else
+			redirect "/login"
+		end
     end 
+
+    get '/logout' do 
+        session.clear
+        redirect "/login"
+    end
 
     helpers do
 		
